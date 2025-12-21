b39980b675dee41f40b8d797a829a4c720cb5b3f
Hans Meyer <hansifer@users.noreply.github.com>
2025-12-21 12:30:40 -0700

    add astro config

---
+++ astro.config.mjs
@@ -0,0 +1,48 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import starlight from '@astrojs/starlight';
+import react from '@astrojs/react';
+import tailwindcss from '@tailwindcss/vite';
+
+import path from 'path';
+import formatClassname from './util/formatClassname';
+import { sidebar } from './sidebar';
+
+export default defineConfig({
+  redirects: {},
+  integrations: [
+    starlight({
+      title: 'Session Buddy',
+      components: {
+        PageFrame: './src/components/CustomStarlightPageFrame.astro',
+      },
+      customCss: [
+        './src/styles/starlight.css',
+        '@fontsource/inter',
+        '@fontsource/inter/500.css',
+        '@fontsource/inter/600.css',
+        '@fontsource/inter/700.css',
+        '@fontsource/inter/800.css',
+        '@fontsource/inter/900.css',
+      ],
+      sidebar,
+    }),
+    react({
+      babel: {
+        plugins: [formatClassname],
+      },
+    }),
+  ],
+  vite: {
+    // @tailwindcss/vite plugin: issue with Vite and plugin typing · Issue #18802 · tailwindlabs/tailwindcss: https://github.com/tailwindlabs/tailwindcss/issues/18802
+    // Astro not working with latest TailwindCSS · Issue #14030 · withastro/astro: https://github.com/withastro/astro/issues/14030
+    // @ts-expect-error
+    plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        '@': path.resolve('./src'),
+      },
+    },
+  },
+});
---
+++ util/formatClassname.ts
@@ -0,0 +1,36 @@
+import type { NodePath } from '@babel/core';
+import * as t from '@babel/types';
+
+export default function formatClassname() {
+  return {
+    visitor: {
+      JSXAttribute(path: NodePath<t.JSXAttribute>) {
+        if (path.node.name.name !== 'className') return;
+
+        const value = path.node.value;
+        if (!value) return;
+
+        // Handle: className="..."
+        if (value.type === 'StringLiteral') {
+          value.value = flattenClassname(value.value);
+        }
+
+        // Handle: className={`...`}
+        if (
+          value.type === 'JSXExpressionContainer' &&
+          value.expression.type === 'TemplateLiteral' &&
+          value.expression.expressions.length === 0
+        ) {
+          path.node.value = {
+            type: 'StringLiteral',
+            value: flattenClassname(value.expression.quasis[0].value.raw),
+          };
+        }
+      },
+    },
+  };
+}
+
+function flattenClassname(classname: string) {
+  return classname.trim().replace(/\s+/g, ' ');
+}

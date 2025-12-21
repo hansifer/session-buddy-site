149b6c3e90d17cd5cfccd1394d5e80da7f7e714d
Hans Meyer <hansifer@users.noreply.github.com>
2025-12-21 12:48:46 -0700

    add starlight sidebar config

---
+++ sidebar.ts
@@ -0,0 +1,80 @@
+import starlight from '@astrojs/starlight';
+
+type Sidebar = Parameters<typeof starlight>[0]['sidebar'];
+
+// use explicit sidebar config because starlight's sidebar autogen does not handle groups well (index.md sidebar.order is not reliable, label comes from folder name, etc)
+export const sidebar: Sidebar = [
+  {
+    label: 'Getting started',
+    autogenerate: {
+      directory: 'docs',
+    },
+  },
+  {
+    label: 'Tabs',
+    autogenerate: {
+      directory: 'tabs',
+    },
+  },
+  {
+    label: 'Collections',
+    autogenerate: {
+      directory: 'collections',
+    },
+  },
+  {
+    label: 'History',
+    autogenerate: {
+      directory: 'history',
+    },
+  },
+  {
+    label: 'Other features',
+    autogenerate: {
+      directory: 'other-features',
+    },
+  },
+  {
+    label: 'Configuration',
+    autogenerate: {
+      directory: 'configuration',
+    },
+  },
+  {
+    label: 'Data safeguards',
+    autogenerate: {
+      directory: 'data-safeguards',
+    },
+  },
+  {
+    label: 'Troubleshooting',
+    autogenerate: {
+      directory: 'troubleshooting',
+    },
+  },
+  {
+    label: 'Reference',
+    autogenerate: {
+      directory: 'reference',
+    },
+  },
+  {
+    slug: 'faq',
+  },
+  {
+    slug: 'releases',
+  },
+  {
+    slug: 'roadmap',
+  },
+  {
+    slug: 'older-versions',
+  },
+  {
+    label: 'Privacy',
+    slug: 'privacy',
+  },
+  {
+    slug: 'terms',
+  },
+];

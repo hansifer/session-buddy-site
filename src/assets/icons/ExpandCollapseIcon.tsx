bab82266232586ca76f3b01fe5aeef8b11616a89
Hans Meyer <hansifer@users.noreply.github.com>
2026-01-05 10:23:39 -0700

    add expand/collapse icon

---
+++ src/assets/icons/ExpandCollapseIcon.tsx
@@ -0,0 +1,52 @@
+import { tw } from '@/util/tailwind';
+
+export const ExpandCollapseIcon = ({
+  expanded,
+  className = '',
+}: {
+  expanded: boolean;
+  className: string;
+}) => (
+  <div
+    className={tw`
+    absolute
+    top-6
+    right-4
+    sm:top-8
+    sm:right-8
+    ${className}
+    `}
+  >
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 20 20"
+      width="28px"
+      height="30px"
+      fill="none"
+      className={tw`
+          transition-transform
+          duration-250
+          ${expanded ? 'rotate-180' : 'rotate-90'}
+          `}
+    >
+      <path
+        d="M4.16732 12.5L10.0007 6.66667L15.834 12.5"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  </div>
+);
+
+export const HighlightedExpandCollapseIcon = ({
+  expanded,
+}: {
+  expanded: boolean;
+}) => (
+  <ExpandCollapseIcon
+    expanded={expanded}
+    className="text-secondaryColor"
+  />
+);

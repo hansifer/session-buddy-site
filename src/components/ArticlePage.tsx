f09908ac092f538ebd373c8ea7292dfffd8e2c5a
Hans Meyer <hansifer@users.noreply.github.com>
2026-01-06 15:35:35 -0700

    add ArticlePage comp

---
+++ src/components/ArticlePage.tsx
@@ -0,0 +1,113 @@
+import { formatDate } from '@/util/date';
+import type { Article } from '@/types';
+
+export const ArticlePage = ({
+  article: { title, subtitle, date, image, content, author },
+}: {
+  article: Article;
+}) => {
+  return (
+    <div
+      className="
+        flex
+        justify-center
+      "
+    >
+      <div
+        className="
+          px-2
+          sm:px-4
+        "
+      >
+        <article
+          className="
+            p-8
+            w-full
+            lg:w-300
+            2xl:w-350
+            mb-24
+            text-center
+          "
+        >
+          <header className="mb-12">
+            <div
+              className="
+                my-4
+                text-sm
+                text-secondaryText
+              "
+            >
+              {formatDate(date)}
+            </div>
+            <h1
+              className="
+                mb-4
+                text-[2rem]
+                xs:text-[2.8rem]
+                lg:text-[3.5rem]
+                font-bold
+                text-primaryText
+              "
+            >
+              {title}
+            </h1>
+            <p
+              className="
+                mb-4
+                text-lg
+                text-secondaryText
+              "
+            >
+              {subtitle}
+            </p>
+          </header>
+          <img
+            src={image}
+            alt={title}
+            className="
+              my-8
+              w-11/12
+              md:w-3/4
+              mx-auto
+              rounded-xl
+              sm:rounded-2xl
+              lg:rounded-3xl
+            "
+            aria-label={title}
+          />
+          <section
+            className="
+              w-full
+              md:w-10/12
+              lg:w-2/3
+              mt-16
+              mx-auto
+              sm:text-xl
+              leading-6!
+              sm:leading-8!
+              text-left
+              sm:text-justify
+              text-secondaryText
+            "
+            aria-labelledby="content-title"
+          >
+            {content}
+          </section>
+          <div
+            className="
+              w-4/5
+              lg:w-2/3
+              mt-8
+              mx-auto
+              text-xl
+              text-right
+              text-primaryText
+            "
+          >
+            {`~ ${author}`}
+          </div>
+        </article>
+      </div>
+    </div>
+  );
+};

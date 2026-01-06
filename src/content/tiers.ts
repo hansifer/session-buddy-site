5588c04062cefab030dfab7c1cb5e8b85659828d
Hans Meyer <hansifer@users.noreply.github.com>
2026-01-06 16:51:57 -0700

    add pricing content

---
+++ src/content/tiers.ts
@@ -0,0 +1,36 @@
+import type { Tier } from '@/types';
+
+export const tiers = [
+  {
+    name: 'Starter',
+    description: 'Ideal for individuals with basic needs.',
+    priceMonthly: 0,
+    priceYearly: 0,
+    features: ['All features', 'Community support'],
+  },
+  {
+    name: 'Standard',
+    description: 'Perfect for small teams and growing businesses.',
+    emphasize: true,
+    priceMonthly: 3.99,
+    priceYearly: 39.99,
+    features: [
+      'Everything in Starter',
+      'Cloud storage for up to 2,000 collections',
+      'Email support',
+    ],
+  },
+  {
+    name: 'Pro',
+    description:
+      'The best choice for professionals seeking enhanced capabilities.',
+    priceMonthly: 5.99,
+    priceYearly: 59.99,
+    features: [
+      'Everything in Standard',
+      'Unlimited cloud storage for collections',
+      'Priority email support',
+      'Multi-user license (up to 5 users)',
+    ],
+  },
+] as const satisfies Tier[];

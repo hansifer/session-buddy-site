05a65a156f00692e092c27fb3ac6724e859fe266
Hans Meyer <hansifer@users.noreply.github.com>
2026-03-07 12:06:06 -0700

    adjust content

--- src/components/Features/More.tsx
+++ src/components/Features/More.tsx
@@ -30,7 +30,7 @@ export const More = ({
           'Quickly replace old collection contents with your current tabs.',
           'Keep track of open tabs with a live counter on the toolbar icon.',
           "Navigate previously-selected collections naturally using the browser's back button.",
-          'Enjoy a polished look with robust site icon fallbacks for missing or broken favicons.',
+          'Enjoy a polished look with smart site icon fallbacks for missing or broken favicons.',
           // 'Use powerful organizational features like cloning, merging, and splitting to manage collections and folders.',
           // 'list the current window first',
         ]}

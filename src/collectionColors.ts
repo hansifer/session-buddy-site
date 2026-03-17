5b7ca1cdaccdd99ef52ad13bf4d7c03e489bff13
Hans Meyer <hansifer@users.noreply.github.com>
2026-03-17 17:16:35 -0600

    add collectionColors

---
+++ src/collectionColors.ts
@@ -0,0 +1,12 @@
+// taken from SB `src/components/theme.ts`
+export const COLLECTION_COLORS_DARK = [
+  '#ff4343',
+  '#dd6700',
+  '#e9b80d',
+  '#21aa16',
+  '#00b8aa',
+  '#0078ca',
+  '#ba59de',
+  '#ff4c9a',
+  '#91614a',
+] as const;

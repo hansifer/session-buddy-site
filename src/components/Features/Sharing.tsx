433a1d3089a01cb1da66fe62fae9c4e39a515b84
Hans Meyer <hansifer@users.noreply.github.com>
2026-03-19 09:18:27 -0600

    update images; adjust content

--- src/components/Features/Sharing.tsx
+++ src/components/Features/Sharing.tsx
@@ -2,8 +2,8 @@ import { motion } from 'framer-motion';
 
 import { Feature } from '@/components/Feature/Feature';
 
-import feature5 from '@/assets/images/feature-placeholder-5.jpg';
-import feature6 from '@/assets/images/feature-placeholder-6.jpg';
+import addLinksImg from '@/assets/images/add-links.jpg';
+import exportImg from '@/assets/images/export.jpg';
 
 export const Sharing = ({
   reverse,
@@ -32,22 +32,24 @@ export const Sharing = ({
     >
       <Feature
         heading="Share flexibly"
-        description="Your data belongs to you. Move collections, tabs, and bookmarks freely in and out of Session Buddy with robust import, export, copy, and paste."
+        description="Move collections, tabs, and bookmarks freely in and out of Session Buddy with robust import, export, copy and paste."
         bullets={[
-          'Instantly open any list of URLs as tabs or save them to a collection.',
-          'Copy and paste formatted links directly into emails, documents, or chat.',
-          'Import smartly from raw text with automatic format detection and URL extraction.',
-          'Export to the clipboard or a file.',
+          'Instantly open a list of URLs as tabs or save them to a collection.',
+          'Import any text with automatic format detection and smart URL extraction.',
+          'Copy tabs or bookmarks and paste them directly into emails, documents, or chat.',
+          // 'Export to the clipboard or a file.',
           'Support for 15+ formats including plain text, JSON, CSV, Markdown, and HTML.',
         ]}
         images={[
           {
-            src: feature5.src,
+            src: addLinksImg.src,
             alt: 'Feature image 5',
+            className: 'shadow-2xl shadow-black/80 max-w-116 lg:max-w-100',
           },
           {
-            src: feature6.src,
+            src: exportImg.src,
             alt: 'Feature image 6',
+            className: 'shadow-2xl shadow-black/80 max-w-116 lg:max-w-100',
           },
         ]}
         reverse={reverse}

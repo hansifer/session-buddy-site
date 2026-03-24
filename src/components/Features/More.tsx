1832f4c90f16b7a77c48c2cdbc73b7de016aced8
Hans Meyer <hansifer@users.noreply.github.com>
2026-03-24 09:32:55 -0600

    remove images

--- src/components/Features/More.tsx
+++ src/components/Features/More.tsx
@@ -2,9 +2,6 @@ import { motion } from 'framer-motion';
 
 import { Feature } from '@/components/Feature/Feature';
 
-import feature5 from '@/assets/images/feature-placeholder-5.jpg';
-import feature6 from '@/assets/images/feature-placeholder-6.jpg';
-
 export const More = ({
   reverse,
   band,
@@ -40,16 +37,6 @@ export const More = ({
           "Navigate collections naturally with the browser's back/forth buttons.",
           'Enjoy a polished look with smart fallbacks for broken and outdated site icons.',
           // 'Use powerful organizational features like cloning, merging, and splitting to manage collections and folders.',
-        ]}
-        images={[
-          {
-            src: feature5.src,
-            alt: 'Feature image 5',
-          },
-          {
-            src: feature6.src,
-            alt: 'Feature image 6',
-          },
         ]}
         reverse={reverse}
         band={band}

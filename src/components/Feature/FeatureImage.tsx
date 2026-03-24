7d7c11706a5d0ded5022a8a7c244be46f2946a90
Hans Meyer <hansifer@users.noreply.github.com>
2026-03-24 07:45:32 -0600

    add shadow prop

--- src/components/Feature/FeatureImage.tsx
+++ src/components/Feature/FeatureImage.tsx
@@ -3,20 +3,28 @@ import { tw } from '@/util/tailwind';
 export const FeatureImage = ({
   src,
   alt,
+  shadow,
   className = '',
 }: {
   src: string;
   alt: string;
+  shadow?: boolean;
   className?: string;
 }) => (
-  <div className="p-3 overflow-hidden">
+  <div
+    className={tw`
+      inline-block
+      m-3
+      overflow-hidden
+      rounded-xl
+      ${shadow ? 'shadow-2xl shadow-black/80' : ''}
+    `}
+  >
     <img
       src={src}
       alt={alt}
       aria-label={alt}
       className={tw`
-        rounded-xl
-        mx-auto
         ${className}
       `}
     />

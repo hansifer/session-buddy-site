77492038371afc12b76df517a4bc7b63e6510d7a
Hans Meyer <hansifer@users.noreply.github.com>
2026-03-29 12:40:25 -0600

    add sample sites

---
+++ src/sampleSites.ts
@@ -0,0 +1,103 @@
+export const sampleSites = [
+  {
+    title: 'Google',
+    url: 'https://www.google.com',
+    color: '#4285f4',
+  },
+  {
+    title: 'Reddit',
+    url: 'https://www.reddit.com',
+    color: '#ff4500',
+  },
+  {
+    title: 'YouTube',
+    url: 'https://www.youtube.com',
+    color: '#ff0000',
+  },
+  {
+    title: 'News',
+    url: 'https://news.google.com',
+    color: '#e91e63',
+  },
+  {
+    title: 'Amazon',
+    url: 'https://www.amazon.com',
+    color: '#ff9900',
+  },
+  {
+    title: 'Wikipedia',
+    url: 'https://www.wikipedia.org',
+    color: '#aaa',
+  },
+  {
+    title: 'Spotify',
+    url: 'https://www.spotify.com',
+    color: '#1db954',
+  },
+  {
+    title: 'LinkedIn',
+    url: 'https://www.linkedin.com',
+    color: '#0077b5',
+  },
+  {
+    title: 'Drive',
+    url: 'https://www.google.com/drive/',
+    color: '#0f9d58',
+  },
+  {
+    title: 'Outlook',
+    url: 'https://outlook.live.com',
+
+    color: '#0078d4',
+  },
+  {
+    title: 'Facebook',
+    url: 'https://www.facebook.com',
+    color: '#1877f2',
+  },
+  {
+    title: 'Instagram',
+    url: 'https://www.instagram.com',
+    color: '#e4405f',
+  },
+  {
+    title: 'Gmail',
+    url: 'https://mail.google.com',
+    color: '#f4b400',
+  },
+  {
+    title: 'Calendar',
+    url: 'https://calendar.google.com',
+    color: '#ff6f00',
+  },
+  {
+    title: 'Pinterest',
+    url: 'https://www.pinterest.com',
+    color: '#e60023',
+  },
+  {
+    title: 'Kickstarter',
+    url: 'https://www.kickstarter.com',
+    color: '#20beff',
+  },
+  {
+    title: 'GitHub',
+    url: 'https://www.github.com',
+    color: '#aaa',
+  },
+  {
+    title: 'Claude',
+    url: 'https://www.claude.ai',
+    color: '#f25022',
+  },
+  {
+    title: 'Hulu',
+    url: 'https://www.hulu.com',
+    color: '#0f9d58',
+  },
+  {
+    title: 'Slack',
+    url: 'https://slack.com',
+    color: '#b400b7',
+  },
+] as const;

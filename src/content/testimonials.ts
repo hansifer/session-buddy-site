a67f43615aea07cea076f0550a416a79f274684c
Hans Meyer <hansifer@users.noreply.github.com>
2026-04-01 11:22:06 -0600

    add testimonials

--- src/content/testimonials.ts
+++ src/content/testimonials.ts
@@ -1,25 +1,194 @@
 import type { Testimonial } from '@/types';
 
-import davidWilsonImage from '@/assets/images/collection.png';
+import drieschDeNoronha from '@/assets/profile-images/driesch-de-noronha.jpg';
+import kenWu from '@/assets/profile-images/ken-wu.png';
+import jonE from '@/assets/profile-images/jon-e.jpg';
+import mohamedHasaan from '@/assets/profile-images/mohamed-hasaan.png';
+import anderWriter from '@/assets/profile-images/anderwriter.jpg';
+import maxButler from '@/assets/profile-images/max-butler.png';
+import barryGrau from '@/assets/profile-images/barry-grau.png';
+import dylanDevens from '@/assets/profile-images/dylan-devens.png';
+import warrenShigoli from '@/assets/profile-images/warren-shigoli.png';
+import cR from '@/assets/profile-images/c-r.jpg';
+import reinoReent from '@/assets/profile-images/reino-reent.png';
+import joeBtfsplk from '@/assets/profile-images/joe-btfsplk.jpg';
+import vítPokorný from '@/assets/profile-images/vít-pokorný.jpg';
+import markMcCormick from '@/assets/profile-images/mark-mccormick.png';
+import lovelyMangla from '@/assets/profile-images/lovely-mangla.jpg';
+import jugalJadhav from '@/assets/profile-images/jugal-jadhav.jpg';
+import ronalAbraham from '@/assets/profile-images/ronal-abraham.png';
+import jeffSchaefer from '@/assets/profile-images/jeff-schaefer.jpg';
+import valters from '@/assets/profile-images/valters.png';
+import nikhilKundra from '@/assets/profile-images/nikhil-kundra.png';
+import johnToumpis from '@/assets/profile-images/john-toumpis.jpg';
+import jordanRoberts from '@/assets/profile-images/jordan-roberts.png';
+import ryanFerguson from '@/assets/profile-images/ryan-ferguson.jpg';
+import knightMayne from '@/assets/profile-images/knight-mayne.jpg';
+import grantMiller from '@/assets/profile-images/grant-miller.jpg';
+import anon from '@/assets/profile-images/anon.jpg';
+import nicoleWest from '@/assets/profile-images/nicole-west.png';
 
 export const testimonials = [
   {
-    name: 'David Wilson',
-    role: 'Founder of Initech',
+    name: 'Jordan Roberts',
+    image: jordanRoberts.src,
     quote:
-      'Session Buddy has transformed the way I manage my online sessions. Its intuitive interface and powerful features have made scheduling and organizing sessions a breeze. Highly recommended!',
-    image: davidWilsonImage.src,
+      'One of the goat extensions. Never buggy, easy to use, tons of functionality, great UI, constant updates. All around great extension.',
   },
   {
-    name: 'Sarah Lee',
-    role: 'Founder',
-    company: 'Initech',
+    name: 'Nikhil Kundra',
+    image: nikhilKundra.src,
     quote:
-      'As a team leader, Session Buddy has been an invaluable tool for coordinating our remote sessions. The seamless integration with our calendars and communication platforms has improved our productivity and collaboration significantly.',
+      'Best damn tab organizer out there. Fast, intuitive, lightweight. An absolute life-saver.',
   },
   {
-    name: 'Michael Chen',
+    name: 'Driesch De Noronha',
+    image: drieschDeNoronha.src,
+    // role: 'Founder of Initech',
     quote:
-      "I can't imagine going back to my old session management methods after using Session Buddy. The convenience of having all my sessions organized in one place, along with the smart reminders, has made a huge difference in my daily workflow.",
+      'This is an example of an app that just works! Honestly mate, the design and interface are so intuitive and simple to use, I love it.',
+  },
+  {
+    name: 'Ken Wu',
+    image: kenWu.src,
+    // role: 'Founder',
+    // company: 'Initech',
+    quote:
+      "As someone who's prone to accumulating way too many tabs, this extension is a godsend.",
+  },
+  {
+    name: 'Jon E',
+    image: jonE.src,
+    quote:
+      "The best bookmark manager out there ... the only one that doesn't violate your privacy or sell your data, and offers the most complete functionality.",
+  },
+  {
+    name: 'Mohamed Hasaan',
+    image: mohamedHasaan.src,
+    quote: 'The greatest bookmark manager out there!',
+  },
+  {
+    name: 'AnderWriter',
+    image: anderWriter.src,
+    quote:
+      "I think this add-on is excellently written. I've switched to it from my other long-time tab manager.",
+  },
+  {
+    name: 'Max Butler',
+    image: maxButler.src,
+    quote:
+      'An absolute must have for any browser. Incredibly easy to use and it is so much better than just using bookmarks and tab/window management.',
+  },
+  {
+    name: 'Barry Grau',
+    image: barryGrau.src,
+    quote: 'I love this app!',
+  },
+  {
+    name: 'Dylan Devens',
+    image: dylanDevens.src,
+    quote:
+      'Should be default on chrome, able to restore sessions that would otherwise be lost.',
+  },
+  {
+    name: 'Warren Shigoli',
+    image: warrenShigoli.src,
+    quote: 'The best thing on chrome!',
+  },
+  {
+    name: 'C R',
+    image: cR.src,
+    quote:
+      "Awesome little tool; it's one of those things one thinks should be an integral part of any browser.",
+  },
+  {
+    name: 'Reino Reent',
+    image: reinoReent.src,
+    quote:
+      'I’ve been using Session Buddy for at least five years and it’s one of the best and most useful extensions out there. I honestly couldn’t live without it.',
+  },
+  {
+    name: 'Joe Btfsplk',
+    image: joeBtfsplk.src,
+    quote:
+      "I've used Session Buddy since forever. It never ever lets me down. This is such a reliable tool. Love it.",
+  },
+  {
+    name: 'Vít Pokorný',
+    image: vítPokorný.src,
+    quote:
+      'Absolutely useful, I am using it for several years now, it works, it is simple and elegant. Thx.',
+  },
+  {
+    name: 'Mark McCormick',
+    image: markMcCormick.src,
+    quote:
+      'Great extension, and exactly what I was looking for. Session buddy will store all the open tabs so they can be restored after a shutdown when Chrome forgets them.',
+  },
+  {
+    name: 'Lovely Mangla',
+    image: lovelyMangla.src,
+    quote:
+      'This extension makes managing dozens of tabs so much easier. I can save groups, search through them quickly, and free up memory in just a click.',
+  },
+  {
+    name: 'Jugal Jadhav',
+    image: jugalJadhav.src,
+    quote:
+      'Session Buddy has saved me countless times by restoring tabs after a crash. I also love how I can neatly organize sessions for later without cluttering my browser.',
+  },
+  {
+    name: 'Ronal Abraham',
+    image: ronalAbraham.src,
+    quote:
+      'Ever feel guilty about closing unread tabs? Want to keep those tabs around forever? Then this is the extension for you! I love it.',
+  },
+  {
+    name: 'Jeff Schaefer',
+    image: jeffSchaefer.src,
+    quote:
+      "One of the best extensions for chrome of all time and I'm talking top 10.",
+  },
+  {
+    name: 'Valters',
+    image: valters.src,
+    quote:
+      'Had alot of pinned tabs and lost all of them ... Opened [Session Buddy] and boom - one click and it was all back!',
+  },
+  {
+    name: 'John Toumpis',
+    image: johnToumpis.src,
+    quote:
+      "This extension saved me from disaster! It restored 70 tabs from a Chrome window I thought I'd lost forever.",
+  },
+  {
+    name: 'Ryan Ferguson',
+    image: ryanFerguson.src,
+    quote:
+      'Fantastic tool, I use for everyday work and even on my personal Chrome browser. LOVE IT!',
+  },
+  {
+    name: 'Knight Mayne',
+    image: knightMayne.src,
+    quote:
+      'I seriously cannot stress how much I love this extension. It has saved my history more times than I can count. Highly recommended.',
+  },
+  {
+    name: 'Grant Miller',
+    image: grantMiller.src,
+    quote:
+      'Easily my favorite Chrome plugin, the history feature, saving just certain windows, it does everything.',
+  },
+  {
+    name: 'Anon',
+    image: anon.src,
+    quote:
+      'No substance could ever hit me with that much dopamine as when i discovered that past me had installed session buddy and it saved all my tabs.',
+  },
+  {
+    name: 'Nicole West',
+    image: nicoleWest.src,
+    quote:
+      "I'm so glad this exists. I love my tabs, but until now, they have never loved me back <3",
   },
 ] as const satisfies Testimonial[];

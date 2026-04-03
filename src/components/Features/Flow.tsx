8f679b5a1fd7a891fa9aba0a57f39c4586f776ac
Hans Meyer <hansifer@users.noreply.github.com>
2026-04-03 13:13:06 -0600

    refactor

--- src/components/Features/Flow.tsx
+++ src/components/Features/Flow.tsx
@@ -9,53 +9,55 @@ export const Flow = ({
 }: {
   reverse?: boolean;
   band?: 'diagonal' | true | false;
-}) => (
-  <section className="w-full">
-    <div
-      id="features"
-      className="
+}) => {
+  return (
+    <section className="w-full">
+      <div
+        id="features"
+        className="
         absolute
         -mt-20
         lg:-mt-32
       "
-    />
-    <motion.div
-      initial={{ opacity: 0 }}
-      whileInView={{ opacity: 1 }}
-      viewport={{ once: true }}
-      transition={{
-        duration: 0.5,
-        delay: 0.2,
-      }}
-    >
-      <Feature
-        heading="Flow seamlessly between tabs and bookmarks"
-        description="Instantly clear tab clutter and free up memory. Save open tabs as a collection with a single click and restore them exactly as you left them."
-        bullets={[
-          'Save or restore full sessions or cherry-pick just the tabs you need.',
-          'Restore tabs flexibly to original windows, a single window, or the current window.',
-          'Preserve workspace context with window positions, pinned tabs, and incognito states fully restored.',
-          'Manage both tabs and bookmarks together in one unified interface.',
-        ]}
-        images={
-          <div
-            className="
+      />
+      <motion.div
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        viewport={{ once: true }}
+        transition={{
+          duration: 0.5,
+          delay: 0.2,
+        }}
+      >
+        <Feature
+          heading="Flow seamlessly between tabs and bookmarks"
+          description="Instantly clear tab clutter and free up memory. Save open tabs as a collection with a single click and restore them exactly as you left them."
+          bullets={[
+            'Save or restore full sessions or cherry-pick just the tabs you need.',
+            'Restore tabs flexibly to original windows, a single window, or the current window.',
+            'Preserve workspace context with window positions, pinned tabs, and incognito states fully restored.',
+            'Manage both tabs and bookmarks together in one unified interface.',
+          ]}
+          images={
+            <div
+              className="
               flex
               justify-center
             "
-          >
-            <CollectionSaveRestore
-              width={600}
-              windowCount={4}
-              minTabs={5}
-              maxTabs={7}
-              // windowVerticalOffset={14}
-            />
-          </div>
-        }
-        reverse={reverse}
-        band={band}
-      />
-    </motion.div>
-  </section>
-);
+            >
+              <CollectionSaveRestore
+                width={600}
+                windowCount={4}
+                minTabs={5}
+                maxTabs={7}
+                // windowVerticalOffset={14}
+              />
+            </div>
+          }
+          reverse={reverse}
+          band={band}
+        />
+      </motion.div>
+    </section>
+  );
+};

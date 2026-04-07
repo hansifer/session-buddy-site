e2badc5c53378f9ce65c217098ddbce847c5067f
Hans Meyer <hansifer@users.noreply.github.com>
2026-04-07 17:08:59 -0600

    define Starlight collection conditionally

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,10 +1,13 @@
+import { ENABLE_STARLIGHT } from '../astro.config.mjs';
 import { defineCollection } from 'astro:content';
 import { docsLoader } from '@astrojs/starlight/loaders';
 import { docsSchema } from '@astrojs/starlight/schema';
 
-export const collections = {
-  docs: defineCollection({
-    loader: docsLoader(),
-    schema: docsSchema(),
-  }),
-};
+export const collections = ENABLE_STARLIGHT
+  ? {
+      docs: defineCollection({
+        loader: docsLoader(),
+        schema: docsSchema(),
+      }),
+    }
+  : null;

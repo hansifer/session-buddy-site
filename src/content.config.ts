7e8c09f87f19a18a0d2c41d757ef0098163de27d
Hans Meyer <hansifer@users.noreply.github.com>
2026-04-07 22:51:03 -0600

    use static imports

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,35 +1,32 @@
 import { defineCollection } from 'astro:content';
+
+// comment out if interim docs enabled
+// import { starlightDocsCollection } from './starlight/docsCollection';
+
+// comment out if starlight docs enabled
 import { glob } from 'astro/loaders';
 import { z } from 'astro/zod';
 
-import { ENABLE_STARLIGHT } from './docsConfig';
+// comment out if interim docs enabled
+// const docs = starlightDocsCollection;
 
-let docs;
-
-if (ENABLE_STARLIGHT) {
-  const { starlightDocsCollection } =
-    await import('./starlight/docsCollection');
-
-  docs = starlightDocsCollection;
-} else {
-  // interim docs
-  docs = defineCollection({
-    loader: glob({
-      pattern: '**/*.{md,mdx}',
-      base: './src/content/docs',
-    }),
-    schema: ({ image }) =>
-      z
-        .object({
-          title: z.string(),
-          description: z.string().optional(),
-          slug: z.string().optional(),
-          image: image().optional(),
-          imageAlt: z.string().optional(),
-        })
-        .passthrough(),
-  });
-}
+// comment out if starlight docs enabled
+const docs = defineCollection({
+  loader: glob({
+    pattern: '**/*.{md,mdx}',
+    base: './src/content/docs',
+  }),
+  schema: ({ image }) =>
+    z
+      .object({
+        title: z.string(),
+        description: z.string().optional(),
+        slug: z.string().optional(),
+        image: image().optional(),
+        imageAlt: z.string().optional(),
+      })
+      .passthrough(),
+});
 
 export const collections = {
   docs,

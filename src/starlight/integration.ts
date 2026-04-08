b901603518778f1012e4e02ef40a13a102e65a93
Hans Meyer <hansifer@users.noreply.github.com>
2026-04-07 18:55:34 -0600

    move sidebar and starlight integration config to own module

--- src/starlight/integration.ts
+++ src/starlight/integration.ts
@@ -3,7 +3,7 @@ import starlight from '@astrojs/starlight';
 type Sidebar = Parameters<typeof starlight>[0]['sidebar'];
 
 // use explicit sidebar config because starlight's sidebar autogen does not handle groups well (index.md sidebar.order is not reliable, label comes from folder name, etc)
-export const sidebar: Sidebar = [
+const sidebar: Sidebar = [
   {
     label: 'Getting started',
     autogenerate: {
@@ -83,3 +83,30 @@ export const sidebar: Sidebar = [
     slug: 'terms',
   },
 ];
+
+export function createStarlightIntegration() {
+  return starlight({
+    title: 'Session Buddy',
+    tableOfContents: false,
+    components: {
+      Head: './src/components/CustomStarlightHead.astro',
+      PageFrame: './src/components/CustomStarlightPageFrame.astro',
+      Search: './src/components/CustomStarlightSearch.astro',
+      MobileTableOfContents: './src/components/CustomStarlightMobileTOC.astro',
+      MobileMenuToggle:
+        './src/components/CustomStarlightMobileMenuToggle.astro',
+      MobileMenuFooter:
+        './src/components/CustomStarlightMobileMenuFooter.astro',
+    },
+    customCss: [
+      './src/styles/starlight.css',
+      '@fontsource/inter',
+      '@fontsource/inter/500.css',
+      '@fontsource/inter/600.css',
+      '@fontsource/inter/700.css',
+      '@fontsource/inter/800.css',
+      '@fontsource/inter/900.css',
+    ],
+    sidebar,
+  });
+}
